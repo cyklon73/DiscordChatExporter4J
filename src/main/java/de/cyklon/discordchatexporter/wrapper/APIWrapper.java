59e7e3d5ab00f518d64b820fc160f375ed41eda6
cyklon73 <cyklon698@gmail.com>
2024-12-20 17:56:55 +0100

    implemented Javacord Wrapper

--- src/main/java/de/cyklon/discordchatexporter/wrapper/APIWrapper.java
+++ src/main/java/de/cyklon/discordchatexporter/wrapper/APIWrapper.java
@@ -9,6 +9,12 @@ import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildMessageChannel;
 import net.dv8tion.jda.api.interactions.components.Component;
+import org.javacord.api.entity.channel.ServerTextChannel;
+import org.javacord.api.entity.message.MessageAttachment;
+import org.javacord.api.entity.message.embed.EmbedAuthor;
+import org.javacord.api.entity.message.embed.EmbedField;
+import org.javacord.api.entity.message.embed.EmbedFooter;
+import org.javacord.api.entity.server.Server;
 
 public interface APIWrapper<PC, GC, G, A, C, E, M, U, R, EA, EF, EFO> {
 
@@ -22,6 +28,10 @@ public interface APIWrapper<PC, GC, G, A, C, E, M, U, R, EA, EF, EFO> {
 
 	static APIWrapper<discord4j.core.object.entity.channel.PrivateChannel, discord4j.core.object.entity.channel.GuildMessageChannel, discord4j.core.object.entity.Guild, Attachment, MessageComponent, Embed, discord4j.core.object.entity.Message, discord4j.core.object.entity.User, Reaction, Embed.Author, Embed.Field, Embed.Footer> discord4j() {
 		return Discord4JWrapper.instance;
+	}
+
+	static APIWrapper<org.javacord.api.entity.channel.PrivateChannel, ServerTextChannel, Server, MessageAttachment, org.javacord.api.entity.message.component.Component, org.javacord.api.entity.message.embed.Embed, org.javacord.api.entity.message.Message, org.javacord.api.entity.user.User, org.javacord.api.entity.message.Reaction, EmbedAuthor, EmbedField, EmbedFooter> javacord() {
+		return JavacordWrapper.instance;
 	}
 
 	ExportableChannel ofPrivateChannel(PC channel);
